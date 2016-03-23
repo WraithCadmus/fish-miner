172e3d9fbec3ed0d78a982051618711a3f830660
WraithCadmus <WraithCadmus@users.noreply.github.com>
2016-03-23 21:19:38 +0000

    Inventory item for fishery added

---
+++ prototypes/item.lua
@@ -0,0 +1,11 @@
+data:extend({
+  {
+    type = "item",
+    name = "fishery",
+    icon = "__fish-miner__/graphics/icon_fishery.png",
+    flags = { "goes-to-quickbar" },
+    subgroup = "extraction-machine",
+    place-result = "fishery",
+    stack_size = 5,
+  }
+})
