bc5e98fb73eb97d520f077f52a58e59b9975af56
WraithCadmus <WraithCadmus@users.noreply.github.com>
2016-03-23 21:29:25 +0000

    Basic recipe and graphics

---
+++ prototypes/recipe.lua
@@ -0,0 +1,14 @@
+data:extend({
+  {
+    type = "recipe",
+    name = "fishery",
+    enabled = "true",
+    ingredients =
+    {
+      {"offshore-pump",1},
+      {"electronic-circuit",5},
+      {"iron-gear-wheel",10},
+    }
+    result = "fishery"
+  }
+})
