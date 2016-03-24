b01bbb49b83bc0c66bb90c5c6d29b256b6992e85
WraithCadmus <WraithCadmus@users.noreply.github.com>
2016-03-24 23:06:42 +0000

    Basic fish-mining now working

--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -1,5 +1,5 @@
---[[data:extend({
-  {
+data:extend({
+  --[[{
     type = "recipe",
     name = "fishery",
     enabled = "true",
@@ -10,5 +10,18 @@
       {"iron-gear-wheel",10},
     }
     result = "fishery"
+  }]]--
+  {
+    type = "recipe",
+    energy_required = 600,
+    category = "crafting-with-fluid",
+    name = "raw-fish",
+    enabled = "true",
+    ingredients =
+    {
+      {type="fluid", name="water", amount=1000}
+    },
+    result = "raw-fish"
+
   }
-})]]--
+})
