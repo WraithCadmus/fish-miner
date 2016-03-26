bbc94252b0ed89e2e1cb6934ec72a72a5d91161d
WraithCadmus <WraithCadmus@users.noreply.github.com>
2016-03-26 21:03:32 +0000

    Animal protein in and not crashing

---
+++ prototypes/fluid.lua
@@ -0,0 +1,15 @@
+data:extend({
+  {
+    type = "fluid",
+    name = "animal-protein",
+    default_temperature = 30,
+    max_temperature = 100,
+    heat_capacity = "1KJ",
+    base_color = {r=0.8, g=0.2, b=0.7},
+    flow_color = {r=1, g=0.2, b=0.9},
+    icon = "__fish-miner__/graphics/animal-protein.png",
+    pressure_to_speed_ratio = 0.1,
+    flow_to_energy_ratio = 0.59,
+  }
+
+})
