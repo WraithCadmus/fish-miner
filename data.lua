e0e1c22d62dcf709c152921d03f106d413e6649f
WraithCadmus <WraithCadmus@users.noreply.github.com>
2016-03-27 15:07:16 +0100

    Inserting some recipes into existing tech

--- data.lua
+++ data.lua
@@ -1,3 +1,7 @@
 require("prototypes.recipe")
 require("prototypes.fluid")
 require("prototypes.item")
+
+--Inserting my recipes into exisiting items
+table.insert(data.raw["technology"]["oil-processing"].effects,{type="unlock-recipe",recipe="animal-rendering"})
+table.insert(data.raw["technology"]["sulfur-processing"].effects,{type="unlock-recipe",recipe="regenerative-serum"})
